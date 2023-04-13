1308c51ed3dc5cd20be2b6607a2ead5be918fa95
Sashamxz <sasha.oom7@gmail.com>
2023-04-13 17:56:23 +0300

    fix(main.tf,tempalte.yaml): also add samconfig.toml Description: add samconfig.toml change main.tf - remove event-bridge section add output section in template.yaml

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,61 +1,43 @@
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
 }
 
 
-# Export Lambda function ARN and S3 bucket ARN as Terraform outputs
+# aws cloudformation stack
+data "aws_cloudformation_stack" "sam_stack" {
+  name = "aws-sam-cli-managed-default"
+}
+
+# Export Lambda function ARN and  as Terraform outputs
 output "lambda_function_arn" {
-  value = data.aws_cloudformation_stack.sam_stack_outputs.outputs["MyFunctionArn"]
+  value = data.aws_cloudformation_stack.sam_stack.outputs["MyFunctionArn"]
 }
 
 
-# Create S3 bucket
+# Create s3 bucket
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "log.file_journal"
 }
 
 
-resource "aws_iam_role_policy_attachment" "s3_lambda_log_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-  role       = aws_iam_role.s3_lambda_log_role.name
-}
+# # Create EventBridge rule
+# data "local_file" "event_json" {
+#   filename = "../events/event.json"
+# }
+
+# resource "aws_cloudwatch_event_rule" "s3_event_rule" {
+#   name          = jsondecode(data.local_file.event_json.content)["name"]
+#   description   = jsondecode(data.local_file.event_json.content)["description"]
+#   event_pattern = jsondecode(data.local_file.event_json.content)["event_pattern"]
+# }
 
 
-# Create EventBridge rule
-locals {
-  event_rule = jsondecode(file("../events/event.json"))
-}
-
-
-resource "aws_cloudwatch_event_rule" "s3_event_rule" {
-  name        = local.event_rule.name
-  description = local.event_rule.description
-  event_pattern = local.event_rule.event_pattern
-}
-
-
-# Create EventBridge target
-resource "aws_cloudwatch_event_target" "lambda_event_target" {
-  rule      = aws_cloudwatch_event_rule.s3_event_rule.name
-  arn       = aws_lambda_function.s3_lambda_log.arn
-  target_id = "lambda-target"
-}
-
-
-# Use SAM to package and deploy Lambda function
-resource "aws_cloudformation_stack" "sam_stack" {
-  name = "sam-stack"
-  template_body = file("${path.module}/template.yaml")
-  capabilities = ["CAPABILITY_IAM"]
-}
-
-
-# Use CloudFormation stack output to get Lambda function ARN
-data "aws_cloudformation_stack" "sam_stack_outputs" {
-  name = aws_cloudformation_stack.sam_stack.name
-  output_key = "MyFunctionArn"
-}
+# # Create EventBridge target
+# resource "aws_cloudwatch_event_target" "lambda_event_target" {
+#   rule      = aws_cloudwatch_event_rule.s3_event_rule.name
+#   arn       = aws_lambda_function.s3_lambda_log.arn
+#   target_id = "lambda-target"
+# }
 
 
 # Use CloudFormation stack output to get S3 bucket ARN
@@ -67,9 +49,6 @@ data "aws_s3_bucket_object" "log_bucket_objects" {
     aws_cloudformation_stack.sam_stack,
   ]
 }
-
-
-
 
 
 output "s3_bucket_arn" {
