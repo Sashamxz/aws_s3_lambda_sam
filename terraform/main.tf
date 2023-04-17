d3e10414aaa61b96da184924380531ceddf45571
Sashamxz <sasha.oom7@gmail.com>
2023-04-17 16:21:39 +0300

    infra(main.tf): change iam roles for eventbridge~ Description: add iam roles

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "log_bucket" {
   bucket = "s3log-file-journal"
 }
 
-# s3 bucket
+# s3 bucket arn
 output "s3_bucket_arn" {
   value = aws_s3_bucket.log_bucket.arn
 }
@@ -41,6 +41,7 @@ resource "aws_cloudwatch_event_rule" "s3_event_rule" {
   name          = element(local.event_json.source, 0)
   description   = local.event_json.detail.eventSource[0]
   event_pattern = jsonencode(local.event_json)
+  
 }
 
 # Create EventBridge target
@@ -50,25 +51,51 @@ resource "aws_cloudwatch_event_target" "lambda_event_target" {
   target_id = "lambda-target"
 }
 
-# Add S3 bucket notification configuration
-resource "aws_s3_bucket_notification_configuration" "this" {
-  rule {
-    status = "Enabled"
-
-    filter {
-      prefix = ""
-
-      tags {
-        Source = aws_s3_bucket.log_bucket.arn
-      }
-    }
-
-    # Add EventBridge target
-    destination {
-      arn = aws_eventbridge_event_bus.this.arn
-    }
-  }
+resource "aws_s3_bucket_notification" "s3_bucket_eventbridge" {
+  bucket      = "s3log-file-journal"
+  eventbridge = true
 }
-resource "aws_eventbridge_event_bus" "my_event_bus" {
-  name = "my-event-bus"
+
+# ROLES
+
+# Define IAM roles
+resource "aws_iam_role" "eventbridge_role" {
+  name_prefix = "eventbridge-role-"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "eventbridge_policy" {
+  name_prefix = "eventbridge-policy-"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "lambda:InvokeFunction"
+        ]
+        Effect   = "Allow"
+        Resource = aws_cloudformation_stack.existing_stack.outputs["LambdaFunctionArn"]
+      }
+    ]
+  })
+}
+
+# ATTACH POLICY TO ROLE
+
+resource "aws_iam_role_policy_attachment" "eventbridge_policy_attachment" {
+  policy_arn = aws_iam_policy.eventbridge_policy.arn
+  role       = aws_iam_role.eventbridge_role.name
 }
